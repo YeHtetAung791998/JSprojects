c844009b7bc634efc88fb199fadf0c77c1056a5a
mtm-yehtetaung <yehtetaung@metateammyanmar.com>
2023-09-05 22:19:20 +0630

    finish quote generator

---
+++ -- template-quote-generator/script.js
@@ -0,0 +1,57 @@
+let apiQuotes = [];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText= document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+   
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+function newQuote() {
+    loading();
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    if(!quote.author){
+        authorText.textContent='Anonymous';
+    }else {
+        authorText.textContent = quote.author;
+    }
+    if(quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    
+    quoteText.textContent = quote.text;
+    complete();
+}
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try{
+      const response = await fetch(apiUrl);
+      apiQuotes = await response.json();
+      authorText.textContent = apiQuotes.quoteAuthor;
+      quoteText.textContent = apiQuotes.quoteText
+      newQuote();
+    } catch(err){
+    }
+}
+getQuotes();
+
+
+//Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent}- ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+newQuoteBtn.addEventListener('click',newQuote)
+twitterBtn.addEventListener('click',tweetQuote)
